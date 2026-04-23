f35b3733a03e260dd58cf8fece02e1e0d22565a6
Joshua Wenning <joshuawenning@gmail.com>
2026-04-23 07:19:12 -0400

    Prepare routes for admin namespace

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
   resource :session
   resources :passwords, param: :token
-  resources :verses
+  resources :verses, only: [ :index, :show ]
+
+  namespace :admin do
+    root "dashboard#index"
+    resources :verses
+  end
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
